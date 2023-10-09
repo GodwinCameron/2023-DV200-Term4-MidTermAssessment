3e1b96e5d29e1bfdd0d5b876ea538ff5da36db26
Godwin Cameron <71267628+GodwinCameron@users.noreply.github.com>
2023-10-09 10:48:41 +0200

    navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,15 @@
 import React from "react";
+import "./navbar.css";
 
 const NavBar  = () => {
     return (
-        <div>
-            <h1>NavBar</h1>
+        <div className="nav">
+            <h1 className="glensoundsTitle">GlenSpares</h1>
+            <div className="navItem activeNavItem">Home</div>
+            <div className="navItem">Shop</div>
+            <div className="navItem">Contact Us</div>
+            <div className="navItem">Cart</div>
+
         </div>
     );
 }
