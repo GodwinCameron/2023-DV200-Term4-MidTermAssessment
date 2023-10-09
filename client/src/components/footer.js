eb5568166b5b32a046dcdc714073f735ce6eb9b8
Tebogo Ramolobeng <221244@virtualwindow.co.za>
2023-10-09 13:10:08 +0200

    commit

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
-import './navbar.css'; // You can include the CSS file if needed
+import './navbar.css';
+
+//paths for the icons
+import Logo from '../assets/icons/Logo.svg';
+import InstagramIcon from '../assets/icons/instagram-dark.svg';
+import FacebookIcon from '../assets/icons/facebook-dark.svg';
 
 const Footer = () => {
   return (
@@ -9,49 +14,62 @@ const Footer = () => {
       <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
         {/* Logo */}
         <div className='logo'>
-          <img src='/assets/icons/Logo.svg' alt='Logo' />
+          <img src={Logo} alt='Logo' width="198" />
         </div>
         {/* Social Icons */}
         <div className='social-link'>
           <a href='https://www.instagram.com/your-instagram-url' className='text-reset'>
-            <img src='/assets/icons/instagram-dark.svg' alt='Instagram' />
+            <img src={InstagramIcon} alt='Instagram' width="60" height="60" />
           </a>
           <a href='https://www.facebook.com/your-facebook-url' className='text-reset'>
-            <img src='/assets/icons/facebook-dark.svg' alt='Facebook' />
+            <img src={FacebookIcon} alt='Facebook' width="60" height="60" />
           </a>
         </div>
       </section>
 
       {/* Footer Content */}
       <section className='footer'>
-        <MDBContainer className='text-center text-md-start mt-5'>
-          <MDBRow className='mt-3'>
-            {/* Company Info */}
-            <MDBCol md="3" lg="3" xl="3" className='footer-column'>
-              <h6 className='text-uppercase fw-bold mb-4'>
-                <MDBIcon icon="gem" className="me-3" />
-                GlenSpares
-              </h6>
-              {/* Add your company info here */}
-            </MDBCol>
-            {/* Products */}
-            <MDBCol md="3" lg="3" xl="3" className='footer-column'>
-              <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
-              {/* Add your product links here */}
-            </MDBCol>
-            {/* Useful Links */}
-            <MDBCol md="3" lg="3" xl="3" className='footer-column'>
-              <h6 className='text-uppercase fw-bold mb-4'>Useful Links</h6>
-              {/* Add your useful links here */}
-            </MDBCol>
-            {/* Contact */}
-            <MDBCol md="3" lg="3" xl="3" className='footer-column'>
-              <h6 className='text-uppercase fw-bold mb-4'>Contact</h6>
-              {/* Add your contact info here */}
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
-      </section>
+  <MDBContainer className='text-center text-md-start mt-5'>
+    <MDBRow className='mt-3'>
+      {/* Column 1: Logo */}
+      <MDBCol md="3" lg="3" xl="3" className='footer-column'>
+        <div className='text-uppercase fw-bold mb-4'>
+          <MDBIcon icon="gem" className="me-3" />
+          GlenSpares
+        </div>
+        {/* Add your company info here */}
+      </MDBCol>
+      {/* Column 2: Products */}
+      <MDBCol md="3" lg="3" xl="3" className='footer-column'>
+        <div className='text-uppercase fw-bold mb-4'>
+          <MDBIcon icon="box" className="me-3" />
+          Products
+        </div>
+        <ul>
+          <li>Ford Mustang Convertible LX</li>
+          <li>Volvo S60</li>
+          <li>Range Rover Sport</li>
+        </ul>
+      </MDBCol>
+      {/* Column 3: Useful Links */}
+      <MDBCol md="3" lg="3" xl="3" className='footer-column'>
+        <div className='text-uppercase fw-bold mb-4'>
+          <MDBIcon icon="link" className="me-3" />
+          Useful Links
+        </div>
+        {/* Add your useful links here */}
+      </MDBCol>
+      {/* Column 4: Contact */}
+      <MDBCol md="3" lg="3" xl="3" className='footer-column'>
+        <div className='text-uppercase fw-bold mb-4'>
+          <MDBIcon icon="address-card" className="me-3" />
+          Contact
+        </div>
+        {/* Add your contact info here */}
+      </MDBCol>
+    </MDBRow>
+  </MDBContainer>
+</section>
 
       {/* Bottom Section */}
       <div className='bottom-section'>
@@ -62,4 +80,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
