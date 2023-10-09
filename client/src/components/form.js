c89f8b83e2d9ac88b6ad57c8c46f7a4497030de1
MareliLourens <221119@virtualwindow.co.za>
2023-10-09 10:19:17 +0200

    Deleted text file

---
+++ client/src/components/form.js
@@ -0,0 +1,28 @@
+<form>
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+  
+  <label>
+    Name:
+    <input type="text" name="name" />
+  </label>
+
+  <input type="submit" value="Submit" />
+</form>
