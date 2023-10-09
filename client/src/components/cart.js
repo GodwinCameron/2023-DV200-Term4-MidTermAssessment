72eac792b935f4af433e2c3efc8a2497459bf1ff
Tristanstormmm <221195@virtualwindow.co.za>
2023-10-09 13:38:56 +0200

    cart_update

--- client/src/components/cart.js
+++ client/src/components/cart.js
@@ -1,16 +1,33 @@
 import React from "react";
-import "../App.css";
+import "./cart.css";
 import NavBar from "./NavBar";
-import Form from "./form_create";
 
 const Cart = () => {
     return (
-        <div className="admin_main">
-
+        <div className="main">
             <NavBar />
-            <div className="forms">
-                <Form />
+
+            <div className="cart-item">
+                <div className="c-image">
+                    <div className="cart-image"></div>
+                </div>
+                <div className="content-2">
+                    <div className="c-content-1">
+                        <div className="cart-price-title">Price</div>
+                        <div className="cart-price">R000000</div>
+                    </div>
+                    <div className="c-content-1">
+                        <div className="cart-quantity-title">Quantity</div>
+                        <div className="cart-quantity">1</div>
+                    </div>
+                    <div className="c-content-1">
+                        <div className="cart-total-title">Subtotal</div>
+                        <div className="cart-total">R000000</div>
+                    </div>
+                </div>
+
             </div>
+
         </div>
     );
 };
