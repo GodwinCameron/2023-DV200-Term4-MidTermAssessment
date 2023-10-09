0ab487906150af361b551669239b2fb937388bb2
Godwin Cameron <71267628+GodwinCameron@users.noreply.github.com>
2023-10-09 10:24:20 +0200

    added NavBar + padding strip

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import NavBar from './components/NavBar';
 import Home from './components/home';
 
 function App() {
   return (
     <>
     <Home/>
+    <NavBar />
     </>
   );
 }
