614d5fe3135e59141259f778501f349868f1359c
Tristanstormmm <221195@virtualwindow.co.za>
2023-10-09 10:49:17 +0200

    updated-css

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,14 +1,34 @@
-import React, {} from 'react';
+import React, { } from 'react';
 import "../App.css";
 import Image from "../images/header-image.png";
 
 const Home = () => {
 
-    return (
-      <div className='main'>
-        <img className='main-img' src={Image}/>
+  return (
+    <div className='main'>
+
+      {/* main image */}
+      <img className='main-img' src={Image} />
+
+      {/* purchase section */}
+      <div className='purchase-section'>
+        <div className='block-1'>
+
+        </div>
+        <div className='block-2'>
+
+        </div>
+        <div className='block-3'>
+
+        </div>
+        <div className='block-4'>
+
+        </div>
+
       </div>
-    );
+
+    </div>
+  );
 
 }
 
