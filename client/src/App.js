10dfd6980365cf6a2756ea30a93f8c058c9d7c3e
Godwin Cameron <71267628+GodwinCameron@users.noreply.github.com>
2023-10-09 13:40:21 +0200

    patched

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/home";
 import Cart from "./components/cart";
 import Admin from "./components/admin";
-<<<<<<< Updated upstream
 import Footer from "./components/footer";
-=======
-
-//import Cart from "./components/cart";
->>>>>>> Stashed changes
 
 function App() {
   return (
